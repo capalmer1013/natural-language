be9f6fcec55a08b4b38f77b9eb9c70fb09fcbba1
cpalmer <capalmer1013@gmail.com>
2016-04-17 15:13:53 -0400

    it is a twitter bot

--- main.py
+++ main.py
@@ -5,18 +5,26 @@ import objects
 import twitter
 from nltk.corpus import treebank
 
+def find(s, ch):
+    return [i for i, ltr in enumerate(s) if ltr == ch]
 
 print 'authorizing'
-api = twitter.Api(consumer_key='x5bCSpVNRktPA8I77CEuiXBAF', consumer_secret='j3y323NvjALJk8JwmoMYQtKKr4nUrCGHQgnjxecI51wduofPeS',
-                  access_token_key='721754140406374400-ONJDR3cBSX5Xy1UBtZxZ5HTNRgB0lfw',
-                  access_token_secret='ht3AeapviyiQ7whquilPv5pKNbmMLul6nsBqeTUmuedt4')
+# api = twitter.Api(consumer_key='x5bCSpVNRktPA8I77CEuiXBAF', consumer_secret='j3y323NvjALJk8JwmoMYQtKKr4nUrCGHQgnjxecI51wduofPeS',
+#                   access_token_key='721754140406374400-ONJDR3cBSX5Xy1UBtZxZ5HTNRgB0lfw',
+#                   access_token_secret='ht3AeapviyiQ7whquilPv5pKNbmMLul6nsBqeTUmuedt4')
+
+api = twitter.Api(consumer_key='sKv44OsnEe7IjYBihLANYRJqd', consumer_secret='JnQld76SytuXVi0FuKhCmM8G2GeziiZMta0ffR5EpWblsLRssM',
+                  access_token_key='707825255121821697-O8PIQ7F0yAYWpT2do56xxUbtWNVRK4c',
+                  access_token_secret='oSIEKyc70gi0g5NYCMm8XBZdN6SF6D643Z6dQggTqnkYy')
 
 print 'getting friends'
 users = api.GetFriends()
 statuses = []
 print 'getting statuses'
 for u in users:
-    statuses.append(u.status.text)
+    timeline = api.GetUserTimeline(u)
+    for each in timeline:
+        statuses.append(each.text)
 
 # token =
 # token_key =
@@ -41,8 +49,11 @@ pat = re.compile(r'([A-Z][^\.!?]*[\.!?])', re.M)
 #         text += line+' '
 
 # listOfSentences = pat.findall(text)
-
-listOfSentences = statuses
+listOfSentences = []
+for status in statuses:
+    if '@' not in status:
+        listOfSentences.append(status)
+#listOfSentences = statuses
 
 # i = 0
 # for sentence in listOfSentences:
@@ -94,8 +105,16 @@ for taggedTokens in listOfTaggedTokens:
     for token in taggedTokens:
         if token not in listOfTaggedWords:
             listOfTaggedWords.append(token)
-for i in range(10):
-    api.PostUpdate(G.generateSentence(listOfTaggedWords, listOfSentenceTags))
+
+tweet = G.generateSentence(listOfTaggedWords, listOfSentenceTags)
+listofAts = find(tweet, '@')
+for index in listofAts:
+    tweet = tweet[:index]+tweet[index+1:]
+
+if len(tweet) > 140:
+    tweet = tweet[:140]
+
+api.PostUpdate(tweet)
     # print G.generateSentence(listOfTaggedWords, listOfSentenceTags)
 print 'hello'
 
