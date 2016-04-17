56a7919ced12ef5bafe8abe4f63f2d1e49bec38f
cpalmer <capalmer1013@gmail.com>
2016-04-17 00:10:53 -0400

    been a while hahaha

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import nltk
 import re
+import objects
 from nltk.corpus import treebank
+
+G = objects.weightedGraph()
+
 nltk.data.path.append('/run/media/cpalmer/WD/nltk_data')
 pat = re.compile(r'([A-Z][^\.!?]*[\.!?])', re.M)
 
@@ -21,13 +25,33 @@ for sentence in listOfSentences:
         del listOfSentences[i+1]
     i += 1
 
+listOfWords = []
 listOfTokens = []
 listOfTaggedTokens = []
+listOfSentenceTags = []
+
 for sentence in listOfSentences:
     listOfTokens.append(nltk.wordpunct_tokenize(sentence))
 
 for token in listOfTokens:
     listOfTaggedTokens.append(nltk.pos_tag(token))
+
+# for token in listOfTokens:
+#     for word in token:
+#         if word not in listOfWords:
+#             listOfWords.append(word)
+
+for sentenceTag in listOfTaggedTokens:
+    tempList = []
+    for word in sentenceTag:
+        if word not in listOfWords:
+            listOfWords.append(word)
+
+        tempList.append(word[1])
+    if tempList not in listOfSentenceTags:
+        listOfSentenceTags.append(tempList)
+
+
 print 'hello'
 
 
---
+++ objects.py
@@ -0,0 +1,66 @@
+class weightedGraph:
+    def __init__(self):
+        self.states = ['<begin>']
+        self.edges = {'<begin>': []}
+        # edges
+        # {fromState: [{weight: rational, data: toState}] }
+
+    def trainStart(self, toState):
+        found = False
+
+        for each in self.edges['<begin>']:
+            if each['data'] == toState:
+                each['weight'].numerator += 1
+                found = True
+
+        if not found:
+            self.edges['<begin>'].append({'weight': rational, 'data': toState})
+            self.edges['<begin>'][-1]['weight'].numerator += 1
+            self.edges['<begin>'][-1]['weight'].denominator += 1
+
+        else:
+            for each in self.edges['<begin>']:
+                each['weight'].denominator += 1
+
+
+    def train(self, fromState, toState):
+        if fromState in self.edges:
+            if toState in self.edges[fromState]:
+
+                i = self.edges[fromState].find(toState)
+                self.edges[fromState][i].numerator += 1
+
+                for outState in self.edges[fromState]:
+                    outState['weight'].denominator += 1
+
+            else:
+
+                self.edges[fromState].append({'weight': rational, 'data': toState})
+                self.edges[fromState][-1]['weight'].numerator += 1
+
+                for outState in self.edges[fromState]:
+                    outState['weight'].denominator += 1
+
+        else:
+            self.edges[fromState] = [{'weight': rational, 'data': toState}]
+
+            for item in self.edges[fromState]:
+                item['weight'].numerator += 1
+                item['weight'].denominator += 1
+
+
+class rational:
+    def __init__(self):
+        self.numerator = 0
+        self.denominator = 0
+
+    def toString(self):
+        return str(self.toFloat())
+
+    def toFloat(self):
+        return float(self.numerator)/float(self.denominator)
+
+    def fromString(self, thisString):
+        index = thisString.find('/')
+        self.numerator = thisString[:index]
+        self.denominator = thisString[index+1:]
